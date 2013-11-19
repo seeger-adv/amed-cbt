6a7205341fd0194af54b70af9fd3e241b8ca6938
Jan Seeger <seegerj@adv-boeblingen.de>
2013-11-19 10:34:22 +0100

    populate database on first use.

---
+++ Lernsoftware/WebContent/WEB-INF/classes/setup.sql
@@ -0,0 +1,4 @@
+/* Benutzer */
+insert into T_USER (username, password) values ('admin', 'd033e22ae348aeb5660fc2140aec35850c4da997');
+
+/* Lektionen */
---
+++ Lernsoftware/WebContent/WEB-INF/classes/teardown.sql
@@ -0,0 +1,4 @@
+drop table t_user cascade constraints PURGE;
+drop table t_chapter cascade constraints PURGE;
+drop table t_lesson cascade constraints PURGE;
+drop table t_question cascade constraints PURGE;
