4c2d34d2d8bc0bdcd58caa87e166584ce6f88fef
Jan Seeger <seegerj@adv-boeblingen.de>
2013-11-19 16:01:31 +0100

    cleanup.

--- Lernsoftware/src/de/adv_boeblingen/seeegerj/amed/lernoftware/view/LessonServlet.java
+++ Lernsoftware/src/de/adv_boeblingen/seeegerj/amed/lernoftware/view/LessonServlet.java
@@ -1,6 +1,7 @@
 package de.adv_boeblingen.seeegerj.amed.lernoftware.view;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,9 +14,10 @@ import de.adv_boeblingen.seeegerj.amed.lernoftware.TemplateRenderer;
 @WebServlet("/Lesson/")
 @SuppressWarnings("serial")
 public class LessonServlet extends HttpServlet {
+	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		new TemplateRenderer(req, "/_template.jtpl").PrintOutput(resp
-				.getWriter());
+		PrintWriter writer = resp.getWriter();
+		new TemplateRenderer(req, "/_template.jtpl").PrintOutput(writer);
 	}
 }
