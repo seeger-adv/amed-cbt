8a8f1e5ff850ea59e5a4d73d5072581a07ef205b
Jan Seeger <seegerj@adv-boeblingen.de>
2013-11-19 10:34:53 +0100

    create EntityManagerFactory only once.

--- Lernsoftware/src/de/adv_boeblingen/seeegerj/amed/lernoftware/controller/DatabaseController.java
+++ Lernsoftware/src/de/adv_boeblingen/seeegerj/amed/lernoftware/controller/DatabaseController.java
@@ -1,13 +1,18 @@
 package de.adv_boeblingen.seeegerj.amed.lernoftware.controller;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DatabaseController {
 	private static final String CONFIGURATION = "lernsoftwareconfig";
+	private static final EntityManagerFactory factory;
+
+	static {
+		factory = Persistence.createEntityManagerFactory(CONFIGURATION);
+	}
 
 	public static EntityManager getEntityManager() {
-		return Persistence.createEntityManagerFactory(CONFIGURATION)
-				.createEntityManager();
+		return factory.createEntityManager();
 	}
 }
