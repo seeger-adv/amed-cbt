6ee64d5d2446047a4e0ab9712fa2bb99770a5441
Jan Seeger <seegerj@adv-boeblingen.de>
2013-11-25 10:50:48 +0100

    fixed 404 error response when selecting a lesson.

--- Lernsoftware/src/de/adv_boeblingen/seeegerj/amed/lernoftware/view/LessonServlet.java
+++ Lernsoftware/src/de/adv_boeblingen/seeegerj/amed/lernoftware/view/LessonServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Lesson/")
+@WebServlet("/Lesson/*")
 @SuppressWarnings("serial")
 public class LessonServlet extends HttpServlet {
 	@Override
