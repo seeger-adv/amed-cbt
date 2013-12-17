da43d4746bb9d1ec6991b0b5f01cec8240630f99
Jan Seeger <seegerj@adv-boeblingen.de>
2013-12-17 17:22:00 +0100

    implemented lesson content rendering.

--- Lernsoftware/src/de/adv_boeblingen/seeegerj/amed/lernoftware/util/PathUtil.java
+++ Lernsoftware/src/de/adv_boeblingen/seeegerj/amed/lernoftware/util/PathUtil.java
@@ -1,7 +1,25 @@
 package de.adv_boeblingen.seeegerj.amed.lernoftware.util;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class PathUtil {
 	public static final String buildQuery(String path) {
 		return "/Lernsoftware/login";
+	}
+
+	public static int retrieveLessonId(HttpServletRequest req) {
+		String pathInfo = req.getPathInfo();
+		String[] lessonSegment = pathInfo.split("/");
+
+		if (lessonSegment.length < 2) {
+			return -1;
+		}
+
+		String lesson = lessonSegment[1];
+		if (lesson == null) {
+			return -1;
+		}
+
+		return Integer.parseInt(lesson);
 	}
 }
--- Lernsoftware/src/de/adv_boeblingen/seeegerj/amed/lernoftware/view/LessonServlet.java
+++ Lernsoftware/src/de/adv_boeblingen/seeegerj/amed/lernoftware/view/LessonServlet.java
@@ -9,6 +9,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import de.adv_boeblingen.seeegerj.amed.lernoftware.controller.ChapterController;
+import de.adv_boeblingen.seeegerj.amed.lernoftware.controller.LessonController;
+import de.adv_boeblingen.seeegerj.amed.lernoftware.model.Chapter;
+import de.adv_boeblingen.seeegerj.amed.lernoftware.model.Lesson;
+import de.adv_boeblingen.seeegerj.amed.lernoftware.util.Constants;
+import de.adv_boeblingen.seeegerj.amed.lernoftware.util.PathUtil;
+import de.adv_boeblingen.seeegerj.amed.lernoftware.util.VariableMap;
+
 @WebServlet("/Lesson/*")
 @SuppressWarnings("serial")
 public class LessonServlet extends HttpServlet {
@@ -16,6 +24,44 @@ public class LessonServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		PrintWriter writer = resp.getWriter();
+
+		VariableMap map = VariableMap.getMappingFromRequest(req);
+		map.put(Constants.CONTENT_PARAM, renderLesson(req));
+
 		new TemplateRenderer(req, "/_template.jtpl").PrintOutput(writer);
+	}
+
+	private String renderLesson(HttpServletRequest req) {
+		int lessonId = PathUtil.retrieveLessonId(req);
+
+		if (lessonId == -1) {
+			return renderDefaultLesson();
+		}
+
+		Lesson firstLesson = LessonController.getLesson(lessonId);
+		if (firstLesson == null) {
+			return renderDefaultLesson();
+		}
+
+		return renderLesson(firstLesson);
+	}
+
+	private String renderLesson(Lesson lesson) {
+		if (lesson == null) {
+			return "";
+		}
+
+		String content = lesson.getContent();
+		if (content != null) {
+			return content;
+		}
+
+		return "";
+	}
+
+	private String renderDefaultLesson() {
+		Chapter firstChapter = ChapterController.getChapters().get(0);
+		Lesson firstLesson = firstChapter.getLessons().get(0);
+		return renderLesson(firstLesson);
 	}
 }
