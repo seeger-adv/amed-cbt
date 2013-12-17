0801a2d66ecf6fc1924539968c9f5e883c03b690
Jan Seeger <seegerj@adv-boeblingen.de>
2013-12-17 17:26:11 +0100

    add quiz servlet.

---
+++ Lernsoftware/src/de/adv_boeblingen/seeegerj/amed/lernoftware/view/QuizServlet.java
@@ -0,0 +1,42 @@
+package de.adv_boeblingen.seeegerj.amed.lernoftware.view;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import de.adv_boeblingen.seeegerj.amed.lernoftware.util.Constants;
+import de.adv_boeblingen.seeegerj.amed.lernoftware.util.VariableMap;
+
+@WebServlet("/Quiz/*")
+@SuppressWarnings("serial")
+public class QuizServlet extends HttpServlet {
+
+	public QuizServlet() {
+		super();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+
+		VariableMap map = VariableMap.getMappingFromRequest(req);
+		map.put(Constants.CONTENT_PARAM, renderQuiz(req));
+
+		new TemplateRenderer(req, "/_template.jtpl").PrintOutput(writer);
+	}
+
+	private String renderQuiz(HttpServletRequest req) {
+		return "";
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+	}
+}
