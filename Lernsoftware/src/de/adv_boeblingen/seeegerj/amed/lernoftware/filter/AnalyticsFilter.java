7b428abb0ed77270f71d99e0f8d7388ee96e15bb
Jan Seeger <jan@alphadev.net>
2013-12-23 17:29:44 +0100

    also collect browser information.

--- Lernsoftware/src/de/adv_boeblingen/seeegerj/amed/lernoftware/filter/AnalyticsFilter.java
+++ Lernsoftware/src/de/adv_boeblingen/seeegerj/amed/lernoftware/filter/AnalyticsFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 
 import com.dmurph.tracking.AnalyticsConfigData;
 import com.dmurph.tracking.JGoogleAnalyticsTracker;
@@ -24,13 +25,20 @@ public class AnalyticsFilter
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException,
 			ServletException {
 
-		final AnalyticsConfigData analyticsConfig = new AnalyticsConfigData(Constants.ANALYTICS_KEY);
+		final AnalyticsConfigData analyticsConfig = createConfig(Constants.ANALYTICS_KEY, (HttpServletRequest) req);
 		JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker(analyticsConfig, GoogleAnalyticsVersion.V_4_7_2);
-		String category = ""; // possible values: lesson, quiz
+		String category = ""; // possible values: lesson, quiz, applet
 		String action = ""; // possible values: show, finished
 		tracker.trackEvent(category, action);
 
 		chain.doFilter(req, resp);
+	}
+
+	private AnalyticsConfigData createConfig(String analyticsKey, HttpServletRequest req) {
+		AnalyticsConfigData config = new AnalyticsConfigData(Constants.ANALYTICS_KEY);
+		config.setUserLanguage(req.getLocale().toString());
+		config.setUserAgent(req.getHeader("User-Agent"));
+		return config;
 	}
 
 	@Override
