691e286158143e64d8a5412d14234533c03b1464
Jan Seeger <jan@alphadev.net>
2013-12-29 12:04:14 +0100

    have the state filter render the quiz progress view. (question state still missing)

--- Lernsoftware/src/de/adv_boeblingen/seegerj/amed/lernsoftware/filter/StateFilter.java
+++ Lernsoftware/src/de/adv_boeblingen/seegerj/amed/lernsoftware/filter/StateFilter.java
@@ -9,13 +9,21 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 
+import de.adv_boeblingen.seegerj.amed.lernsoftware.controller.ChapterController;
+import de.adv_boeblingen.seegerj.amed.lernsoftware.controller.NavigationController;
 import de.adv_boeblingen.seegerj.amed.lernsoftware.misc.Constants;
 import de.adv_boeblingen.seegerj.amed.lernsoftware.misc.VariableMap;
+import de.adv_boeblingen.seegerj.amed.lernsoftware.model.Chapter;
+import de.adv_boeblingen.seegerj.amed.lernsoftware.model.Lesson;
+import de.adv_boeblingen.seegerj.amed.lernsoftware.model.Question;
+import de.adv_boeblingen.seegerj.amed.lernsoftware.util.PathUtil;
 
 @WebFilter(urlPatterns = "/Quiz/*")
 public class StateFilter
 		implements Filter {
+
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException,
 			ServletException {
@@ -29,12 +37,27 @@ public class StateFilter
 	private String renderProgress(ServletRequest req) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("Question:\n");
-		sb.append("<div class=\"bla\" id=\"q1\">1</div>\n");
-		sb.append("<div class=\"bla\" id=\"q2\">2</div>\n");
-		sb.append("<div class=\"bla\" id=\"q3\">3</div>\n");
-		sb.append("<div class=\"bla\" id=\"q4\">4</div>\n");
+
+		int id = PathUtil.retrieveLessonId((HttpServletRequest) req);
+		Chapter chapter = ChapterController.getChapter(id);
+
+		for (Lesson lesson : chapter.getLessons()) {
+			for (Question question : lesson.getQuestions()) {
+				sb.append(renderQuestion(sb, question));
+			}
+		}
 		sb.append("<a href=\"\">skip</a>");
 		return sb.toString();
+	}
+
+	private String renderQuestion(StringBuilder sb, Question question) {
+		String htmlClass = getQuestionState(question);
+		String link = String.format("<a href=\"%s\">%s</a>", NavigationController.getQuizLink(question), count);
+		return String.format("<div class=\"%s\" >%s</div>\n", htmlClass, link);
+	}
+
+	private String getQuestionState(Question question) {
+		return "bla";
 	}
 
 	@Override
