828f499d2dd35b927b7eff828bfe60f9f7bbf648
Jan Seeger <jan@alphadev.net>
2013-12-29 11:22:36 +0100

    add Response queries to StateController.

--- Lernsoftware/src/de/adv_boeblingen/seegerj/amed/lernsoftware/controller/StateController.java
+++ Lernsoftware/src/de/adv_boeblingen/seegerj/amed/lernsoftware/controller/StateController.java
@@ -2,9 +2,13 @@ package de.adv_boeblingen.seegerj.amed.lernsoftware.controller;
 
 import de.adv_boeblingen.seegerj.amed.lernsoftware.model.Chapter;
 import de.adv_boeblingen.seegerj.amed.lernsoftware.model.Lesson;
+import de.adv_boeblingen.seegerj.amed.lernsoftware.model.Question;
+import de.adv_boeblingen.seegerj.amed.lernsoftware.model.Response;
 
 public interface StateController {
 	boolean isCurrentChapter(Chapter chapter);
 
 	boolean isLessonComplete(Lesson lesson);
+
+	Response getResponse(Question question);
 }
--- Lernsoftware/src/de/adv_boeblingen/seegerj/amed/lernsoftware/controller/StateControllerImpl.java
+++ Lernsoftware/src/de/adv_boeblingen/seegerj/amed/lernsoftware/controller/StateControllerImpl.java
@@ -1,8 +1,15 @@
 package de.adv_boeblingen.seegerj.amed.lernsoftware.controller;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
 import de.adv_boeblingen.seegerj.amed.lernsoftware.model.Chapter;
 import de.adv_boeblingen.seegerj.amed.lernsoftware.model.Lesson;
+import de.adv_boeblingen.seegerj.amed.lernsoftware.model.Question;
+import de.adv_boeblingen.seegerj.amed.lernsoftware.model.Response;
 import de.adv_boeblingen.seegerj.amed.lernsoftware.model.User;
+import de.adv_boeblingen.seegerj.amed.lernsoftware.util.DatabaseUtil;
+import de.adv_boeblingen.seegerj.amed.lernsoftware.util.DatabaseUtil.DatabaseRunnable;
 
 public class StateControllerImpl
 		implements StateController {
@@ -21,5 +28,18 @@ public class StateControllerImpl
 	@Override
 	public boolean isLessonComplete(Lesson lesson) {
 		return LessonController.isComplete(this.mUser, lesson);
+	}
+
+	@Override
+	public Response getResponse(final Question question) {
+		return DatabaseUtil.runQuery(new DatabaseRunnable<Response>() {
+			@Override
+			public Response run(EntityManager manager, EntityTransaction transaction) {
+				Response query = new Response();
+				query.setUser(mUser);
+				query.setQuestion(question);
+				return manager.find(Response.class, query);
+			}
+		});
 	}
 }
