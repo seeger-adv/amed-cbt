4e08b482c0b2607bfdfd9825e1a0e2f14debab6f
Jan Seeger <seegerj@adv-boeblingen.de>
2014-02-06 13:39:56 +0100

    changed to migLayout.

--- Lernsoftware/src/de/adv_boeblingen/seegerj/amed/lernsoftware/applets/SubnetApplet.java
+++ Lernsoftware/src/de/adv_boeblingen/seegerj/amed/lernsoftware/applets/SubnetApplet.java
@@ -1,23 +1,20 @@
 package de.adv_boeblingen.seegerj.amed.lernsoftware.applets;
 
 import java.applet.Applet;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.Panel;
 
 import javax.swing.JLabel;
 import javax.swing.JSplitPane;
 import javax.swing.JTextField;
 
+import net.miginfocom.swing.MigLayout;
+
 @SuppressWarnings("serial")
 public class SubnetApplet extends Applet {
-
-	private static final Color backgroundColor = new Color(52, 52, 52);
-	private static final Font font = new Font("Helvetica", Font.BOLD, 12);
-
 	private final JTextField ipInput = new JTextField();
 	private final JTextField nmInput = new JTextField();
+	private final JLabel type = new JLabel();
+	private final JLabel clazz = new JLabel();
 	private final JLabel first = new JLabel();
 	private final JLabel last = new JLabel();
 	private final JLabel sn = new JLabel();
@@ -26,38 +23,41 @@ public class SubnetApplet extends Applet {
 	@Override
 	public void init() {
 		super.init();
-		setBackground(backgroundColor);
-
 		Panel inputPanel = new Panel();
-		inputPanel.setLayout(new GridLayout(0, 1));
+		inputPanel.setLayout(new MigLayout());
 		Panel outputPanel = new Panel();
-		outputPanel.setLayout(new GridLayout(0, 2));
+		outputPanel.setLayout(new MigLayout());
 
-		inputPanel.add(createLabel("IP Address"));
-		inputPanel.add(this.ipInput);
-		inputPanel.add(createLabel("Network Mask"));
-		inputPanel.add(this.nmInput);
+		inputPanel.add(createLabel("IP Address"), "span 2, grow");
+		inputPanel.add(createLabel("Type"), "wrap");
+		inputPanel.add(this.ipInput, "span 2, grow");
+		inputPanel.add(this.type, "wrap");
+		inputPanel.add(createLabel("Network Mask"), "span 2, grow");
+		inputPanel.add(createLabel("Class"), "wrap");
+		inputPanel.add(this.nmInput, "span 2, grow");
+		inputPanel.add(this.clazz, "wrap");
 
-		outputPanel.add(createLabel("Broadcast"));
-		outputPanel.add(this.sn);
+		outputPanel.add(createLabel("Network"));
+		outputPanel.add(this.sn, "wrap");
 		outputPanel.add(createLabel("First Usable"));
-		outputPanel.add(this.first);
+		outputPanel.add(this.first, "wrap");
 		outputPanel.add(createLabel("Last Usable "));
-		outputPanel.add(this.last);
+		outputPanel.add(this.last, "wrap");
 		outputPanel.add(createLabel("Broadcast"));
 		outputPanel.add(this.brd);
 
 		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, inputPanel, outputPanel);
-		splitPane.setBackground(backgroundColor);
 		splitPane.setPreferredSize(getSize());
 		add(splitPane);
 	}
 
 	private static JLabel createLabel(String caption) {
 		JLabel label = new JLabel(caption);
-		label.setFont(font);
-		label.setForeground(Color.white);
 		return label;
+	}
+
+	private String getMode() {
+		return getParameter("mode");
 	}
 
 }
